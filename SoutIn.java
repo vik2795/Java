bd614335eb8af157230c0c66619c652f2be4d1bc
vik2795 <157577386+vik2795@users.noreply.github.com>
2024-01-24 09:08:47 +0100

    Add files via upload

---
+++ SoutIn.java
@@ -0,0 +1,9 @@
+String msg = "I'm happy today.";
+int a = 150;
+int o = 0226;
+int h = 0x96;
+double d = 10;
+System.out.PrintIn(msg);
+System.out.PrintIn(a);
+System.out.PrintIn(o);
+System.out.PrintIn(msg);
