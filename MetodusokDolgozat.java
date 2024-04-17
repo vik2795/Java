ce1517324ce2474c4787a3061badc3e9c9c3ff55
vik2795 <157577386+vik2795@users.noreply.github.com>
2024-04-17 08:30:27 +0200

    Add files via upload

---
+++ MetodusokDolgozat.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class  MetodusokDolgozat{
+
+    public  static String signOfTheNumber(int szam) {
+        System.out.println("Adjon meg egy számot!");
+        int szam =0;
+        String result = "";
+        if (szam >= 0)
+        result="1";
+        else if (szam==0)
+        result="0";
+        else if (szam<0)
+        result="-1";
+        else
+        result = "rossz adat";
+    return result;
+        
+    }
+
+
+
+    public  static String twoParameters (int szam2 int szam2_2) {
+        String result = "the meaning of life";
+        System.out.println("Adjon megy egy számot!");
+        int szam2=0;
+        System.out.println("Adjon meg egy másik számot!");
+        int szam2_2=0;
+        
+        return String result = "" +  (int szam2);
+        
+}
+public  static String triangleArea (int szam3 int szam3_3 int szam3_3_3) {
+    String result = "triangleArea";
+    System.out.println("Adja meg a háromszög egy oldalát");
+    int szam3;
+    System.out.println("Adjon meg még egy oldalt!");
+    int szam3_3;
+    System.out.println("Adja meg a harmadik oldalt!");
+    int szam3_3_3;
+    if (szam3 > szam3_3)
+    result="1";
+    else if (szam3 + szam3_3<szam3szam3_3_3)
+    result="1";
+    else  (szam3+szam3_3=szam3_3_3)
+    result="-1";
+    
+        return String result;
+    
+}
+   
