483facfe1cb472cd225d4db74336c5e0b02725f6
György Viktoria Anna <157577386+vik2795@users.noreply.github.com>
2024-11-27 10:48:52 +0100

    Add files via upload

---
+++ Dolgozat-test/Tests.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.beans.Transient;
+import java.util.function.Function;
+ class Tests {
+     Function sc=new Function() {}
+    
+    
+    @Test
+    void dividersNumber()
+    {
+        assertEquals()
+    }
+
+    @Test
+    void maxOfThree()
+    {
+        assertEquals()
+    }
+    @Test
+    void fizzBuzz()
+    {
+        assertThrows(Create new scratch file from selection.class,()->{sc.add("Fizz,Buzz")})
+    }
+    @Test
+    void isVowel()
+    {
+        assertThrows(false)
+    }
+
+
+  
+    }
+
+
+    
+
