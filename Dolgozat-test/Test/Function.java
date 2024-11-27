c685907643c08d5e78e94df5cc06e9fe8178caf4
György Viktoria Anna <157577386+vik2795@users.noreply.github.com>
2024-11-27 10:37:32 +0100

    Add files via upload

---
+++ Dolgozat-test/Test/Function.java
@@ -0,0 +1,49 @@
+package Test;
+
+public class Function {
+    public static int dividersNumber(int numberA, int numberB)
+    {
+        if(numberA%numberB==0)
+    {
+        return String.valueOf(numberA,numberB);
+    }
+
+    }
+
+    public static int maxOfThree(int number1,int number2,int number3)
+    {
+        if(number1 && number2 && number3/ number==0)
+        {
+            return Math.max(number1,number2, number3);
+        }
+    }
+
+public static String fizzbuzz(int number){
+    if(number%3==0 && number%5==0)
+    {
+        return "FizzBuzz";
+    }else if(number%3==0)
+    {
+        return "Fizz";
+    }
+    else if(number%5==0)
+    {
+        return "Buzz";
+    }
+    else
+    return String.valueOf(number);
+    
+    
+}
+
+public static boolean isVowel(String word)
+{
+    boolean=false;
+
+    if(word="a,á,e,é,o,ó,ö,ő,u,ú,ü,ű")
+    {
+        return false
+    }
+    else true
+}
+}
